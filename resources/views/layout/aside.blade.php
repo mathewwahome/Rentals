f1478fea520d3069a1b1259b8b996fb166c9c2d7
mathewwahome <methewwahome@gmail.com>
2024-05-26 00:15:43 +0300

    rent bills

--- resources/views/layout/aside.blade.php
+++ resources/views/layout/aside.blade.php
@@ -13,6 +13,10 @@
                         <li><i class="fa fa-user"></i><a href="{{route('newclients')}}">New Client</a></li>
                     </ul>
                 </li>
+                <li class="menu-title">Houses</li>
+                <li>
+                    <a href="{{route('houses')}}"> <i class="menu-icon fa fa-building-o"></i>Houses </a>
+                </li>
                 <li class="menu-title">Bills</li>
                 <li class="menu-item-has-children dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-money"></i>Bills</a>
@@ -21,10 +25,7 @@
                         <li><i class="menu-icon fa fa-sign-in"></i><a href="{{route('rent')}}">Rent</a></li>
                     </ul>
                 </li> 
-                <li class="menu-title">Houses</li>
-                <li>
-                    <a href="{{route('houses')}}"> <i class="menu-icon fa fa-building-o"></i>Houses </a>
-                </li>
+               
                 {{--  --}}
                
                 <li class="menu-title">Reports</li>
